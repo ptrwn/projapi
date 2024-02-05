c32065bfe88cda5a79e90543b365d8df3d4d181f
ptrwn <1374688@gmail.com>
2024-02-06 00:53:55 +0300

    intermediate results

--- app/main.py
+++ app/main.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from sqlmodel import SQLModel, create_engine, Session, select
-
-from models import Projects
+from models import User, ProjectPost
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
-
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
@@ -26,20 +24,34 @@ app = FastAPI(lifespan=lifespan)
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/projects")
-async def create_project(project: Projects):
+
+@app.get("/user/{user_id}", response_model=User)
+async def get_user(user_id: int):
     with Session(engine) as session:
-        session.add(project)
+        user = session.get(User, user_id)
+    return user
+
+@app.post("/users")
+async def create_user(user: User):
+    with Session(engine) as session:
+        session.add(user)
         session.commit()
-        session.refresh(project)
-        return project
+        session.refresh(user)
+        return user
 
-
-
-@app.get("/projects")
-async def get_all_projects():
+@app.get("/users")
+async def get_all_users():
     with Session(engine) as session:
-        projects = session.exec(select(Projects)).all()
-        return projects
+        users = session.exec(select(User)).all()
+        return users
 
+@app.post("/projects")
+async def create_project(project: ProjectPost):
+    print('===================================')
+    print(project)
 
+    # with Session(engine) as session:
+    #     session.add(project)
+    #     session.commit()
+    #     session.refresh(project)
+    #     return project
--- app/models.py
+++ app/models.py
@@ -3,26 +3,6 @@ from enum import Enum
 from sqlmodel import Field, SQLModel, Relationship
 
 
-class Projects(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str =  Field(default=None, unique=True)
-    description: Optional[str] =  Field(default=None, unique=True)
-    docs: Optional[List["Documents"]] = Relationship(back_populates="project")
-    user_links: Optional[List["UsersProjects"]] = Relationship(back_populates="project")
-
-class Users(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    login: str = Field(default=None, unique=True)
-    password: str = Field(default=None)
-    project_links: Optional[List["UsersProjects"]] = Relationship(back_populates="user")
-
-class Documents(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str =  Field(default=None, unique=True)
-    path: str = Field(default=None, unique=True)
-    project_id: int = Field(default=None, foreign_key="projects.id")
-    project: Projects = Relationship(back_populates="docs")
-
 
 class UserRole(Enum):
     owner: str = "Owner"
@@ -30,15 +10,52 @@ class UserRole(Enum):
 
 
 class UsersProjects(SQLModel, table=True):
-    user_id: Optional[int] = Field(default=None, foreign_key="users.id", primary_key=True)
-    project_id: Optional[int] = Field(default=None, foreign_key="projects.id", primary_key=True)
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id", primary_key=True)
+    project_id: Optional[int] = Field(default=None, foreign_key="project.id", primary_key=True)
     user_role: UserRole = Field(default=UserRole.guest)
-    user: "Users" = Relationship(back_populates="project_links")
-    project: "Projects" = Relationship(back_populates="user_links")
+    users: "User" = Relationship(back_populates="project_links")
+    projects: "Project" = Relationship(back_populates="user_links")
+
+    
+class UserBase(SQLModel):
+    login: str = Field(default=None, unique=True)
+    password: str = Field(default=None)
+class User(UserBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    project_links: Optional[List["UsersProjects"]] = Relationship(back_populates="users")
+class UserPost(UserBase): ...
 
 
-'''
 
-DB rough schema – user (login, hashed password), project (name, description), access between projects and user (user_id, project_id, access_type), documents (doc_id, path / s3 key, project_id) 
 
-'''
+class ProjectBase(SQLModel):
+    name: str =  Field(default=None, unique=True)
+    description: Optional[str] =  Field(default=None, unique=True)
+class Project(ProjectBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    # docs: Optional[List["Documents"]] = Relationship(back_populates="project")
+    user_links: List["UsersProjects"] = Relationship(back_populates="projects")
+
+
+class ProjectPost(ProjectBase):
+    owner_id: int
+    guests: Optional[List[int]]
+    
+
+
+
+
+
+
+
+# class Documents(SQLModel, table=True):
+#     id: Optional[int] = Field(default=None, primary_key=True)
+#     name: str =  Field(default=None, unique=True)
+#     path: str = Field(default=None, unique=True)
+#     project_id: int = Field(default=None, foreign_key="projects.id")
+#     project: Projects = Relationship(back_populates="docs")
+
+
+
+
+
