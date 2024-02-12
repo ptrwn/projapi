27c2fb9c03f44998ced16dc85c6b2c17c21ca554
ptrwn <1374688@gmail.com>
2024-02-12 23:36:31 +0300

    added document into models

--- app/models.py
+++ app/models.py
@@ -31,27 +31,24 @@ class ProjectBase(SQLModel):
     description: Optional[str] =  Field(default=None, unique=True)
 class Project(ProjectBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    # docs: Optional[List["Documents"]] = Relationship(back_populates="project")
+    docs: Optional[List["Document"]] = Relationship(back_populates="project")
     user_links: List["UsersProjects"] = Relationship(back_populates="projects")
 
 
 class ProjectPost(ProjectBase):
     owner_id: int
     guests: Optional[List[int]]
-    
 
 
+class DocumentBase(SQLModel):
+    name: str =  Field(default=None, unique=True)
+    path: str = Field(default=None, unique=True)
 
 
-
-
-
-# class Documents(SQLModel, table=True):
-#     id: Optional[int] = Field(default=None, primary_key=True)
-#     name: str =  Field(default=None, unique=True)
-#     path: str = Field(default=None, unique=True)
-#     project_id: int = Field(default=None, foreign_key="projects.id")
-#     project: Projects = Relationship(back_populates="docs")
+class Document(DocumentBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    project_id: int = Field(default=None, foreign_key="project.id")
+    project: Project = Relationship(back_populates="docs")
 
 
 
