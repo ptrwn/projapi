b8bdb431ca737c911e9e45380b49db00fde2e038
ptrwn <1374688@gmail.com>
2024-03-21 14:28:16 +0300

    ran linter

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from sqlmodel import SQLModel, create_engine, Session, select
-from models import User, UserPost, ProjectPost, Project, UsersProjects, UserRole
+
+from fastapi import FastAPI
+from models import Project, ProjectPost, User, UserPost, UserRole, UsersProjects
+from sqlmodel import Session, SQLModel, create_engine, select
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
@@ -18,7 +20,9 @@ async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
 
+
 app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
 async def root():
@@ -31,14 +35,16 @@ async def get_user(user_id: int):
         user = session.get(User, user_id)
     return user
 
+
 @app.post("/users")
 async def create_user(user: UserPost):
-    user = User(login=user.login, password=user.password)    
+    user = User(login=user.login, password=user.password)
     with Session(engine) as session:
         session.add(user)
         session.commit()
         session.refresh(user)
         return user
+
 
 @app.get("/users")
 async def get_all_users():
@@ -49,11 +55,12 @@ async def get_all_users():
 
 @app.post("/projects")
 async def create_project(project: ProjectPost):
-
-    with Session(engine) as session: 
+    with Session(engine) as session:
         owner = session.get(User, project.owner_id)
         new_project = Project(name=project.name, description=project.description)
-        owner_project = UsersProjects(users=owner, projects=new_project, user_role=UserRole.owner)
+        owner_project = UsersProjects(
+            users=owner, projects=new_project, user_role=UserRole.owner
+        )
         session.add(owner)
         session.add(new_project)
         session.add(owner_project)
@@ -61,27 +68,29 @@ async def create_project(project: ProjectPost):
 
         if project.guests:
             guests = [session.get(User, guest) for guest in project.guests]
-            guests_project = [UsersProjects(users=guest, projects=new_project, user_role=UserRole.guest) for guest in guests]
+            guests_project = [
+                UsersProjects(
+                    users=guest, projects=new_project, user_role=UserRole.guest
+                )
+                for guest in guests
+            ]
 
             for guest, guest_proj in zip(guests, guests_project):
                 session.add(guest)
                 session.add(guest_proj)
             session.commit()
-            
+
 
 @app.put("/project/{project_id}")
-async def update_project(project_id):
-    ...
+async def update_project(project_id): ...
 
 
 @app.get("/project/{project_id}")
-async def get_project(project_id):
-    ...
+async def get_project(project_id): ...
 
 
 @app.get("/projects/{user_id}")
 async def get_projects_for_user(user_id):
-
     res = []
 
     with Session(engine) as session:
@@ -89,10 +98,12 @@ async def get_projects_for_user(user_id):
         for proj_link in user.project_links:
             role = proj_link.user_role.value
             project = session.get(Project, proj_link.project_id)
-            res.append({'role': role, 'project_name': project.name, 'project_desription': project.description})
-        
-    
+            res.append(
+                {
+                    "role": role,
+                    "project_name": project.name,
+                    "project_desription": project.description,
+                }
+            )
+
     return res
-
-
-        
--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
-from typing import Optional, List
 from enum import Enum
-from sqlmodel import Field, SQLModel, Relationship
+from typing import List, Optional
 
+from sqlmodel import Field, Relationship, SQLModel
 
 
 class UserRole(Enum):
@@ -10,25 +10,37 @@ class UserRole(Enum):
 
 
 class UsersProjects(SQLModel, table=True):
-    user_id: Optional[int] = Field(default=None, foreign_key="user.id", primary_key=True)
-    project_id: Optional[int] = Field(default=None, foreign_key="project.id", primary_key=True)
+    user_id: Optional[int] = Field(
+        default=None, foreign_key="user.id", primary_key=True
+    )
+    project_id: Optional[int] = Field(
+        default=None, foreign_key="project.id", primary_key=True
+    )
     user_role: UserRole = Field(default=UserRole.guest)
     users: "User" = Relationship(back_populates="project_links")
     projects: "Project" = Relationship(back_populates="user_links")
 
-    
+
 class UserBase(SQLModel):
     login: str = Field(default=None, unique=True)
     password: str = Field(default=None)
+
+
 class User(UserBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    project_links: Optional[List["UsersProjects"]] = Relationship(back_populates="users")
+    project_links: Optional[List["UsersProjects"]] = Relationship(
+        back_populates="users"
+    )
+
+
 class UserPost(UserBase): ...
 
 
 class ProjectBase(SQLModel):
-    name: str =  Field(default=None, unique=True)
-    description: Optional[str] =  Field(default=None, unique=True)
+    name: str = Field(default=None, unique=True)
+    description: Optional[str] = Field(default=None, unique=True)
+
+
 class Project(ProjectBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     docs: Optional[List["Document"]] = Relationship(back_populates="project")
@@ -41,7 +53,7 @@ class ProjectPost(ProjectBase):
 
 
 class DocumentBase(SQLModel):
-    name: str =  Field(default=None, unique=True)
+    name: str = Field(default=None, unique=True)
     path: str = Field(default=None, unique=True)
 
 
@@ -49,8 +61,3 @@ class Document(DocumentBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     project_id: int = Field(default=None, foreign_key="project.id")
     project: Project = Relationship(back_populates="docs")
-
-
-
-
-
